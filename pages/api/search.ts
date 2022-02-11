d7f2eb083e5ddd20ceb817b028d21cd8fb51e8eb
jeffreydyao <z5316953@ad.unsw.edu.au>
2022-02-11 13:32:09 +1100

    Implemented YouTube search - pending quota increase

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,6 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getNowPlaying, getTracks, getCurrentTrack } from "../../lib/spotify";
 import musicKitToken from "../../lib/musicKitToken";
+import { google } from "googleapis";
+
+// TODO: Integrate YouTube search using block comment
 
 export default async function handler(
   req: NextApiRequest,
@@ -13,6 +16,7 @@ export default async function handler(
 
   let tracks;
   let nowPlayingTrack;
+  let googleAuth;
 
   // Import logic from /tracks API route to return tracks
   if (nowPlaying.status === 204 || nowPlaying.status > 400) {
@@ -52,6 +56,37 @@ export default async function handler(
   )
 
   console.log(appleMusicTracks)
+
+/*   googleAuth = new google.auth.GoogleAuth({
+    credentials: {
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY
+    },
+    scopes: ['https://www.googleapis.com/auth/youtube.readonly']
+  });
+
+  const youtube = google.youtube({
+    auth: googleAuth,
+    version: 'v3'
+  })
+
+  const youtub = await Promise.all(
+    queryData.map((track: any) =>
+      youtube.search.list({
+        // @ts-expect-error
+        // TODO: Work out overload error
+        part: 'snippet',
+        q: `${track.query}`,
+        maxResults: '1',
+      })
+        .then((data: any) => {
+          // TODO: Return only videoId and join with youtube.com/watch?{videoId}
+          // TODO: Request additional quota and integrate YouTube search once complete
+          return JSON.stringify(data.data)
+        })
+
+    )
+  ) */
   
   return res.status(200).json({ appleMusicTracks });
 
